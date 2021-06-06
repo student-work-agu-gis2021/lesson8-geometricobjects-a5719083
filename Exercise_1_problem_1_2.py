1dd2ef009bfa5c332653e050c69750cf13c231db
a5719083 <>
2021-06-06 16:27:49 +0000

    create_line_geom

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -11,7 +11,12 @@
 
 from shapely.geometry import Point, LineString, Polygon
 #YOUR CODE HERE 1 to define create_point_geom()
-def create_point_geom(x_coord,y_coord)
+def create_point_geom(x_coord,y_coord):
+  
+  prameter : x,y
+  Purpose  : create Point
+  point = Point(x_coord,y_coord)
+  return(point)
 # Test your function by running these code cells:
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -27,7 +32,7 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
-
+def create_line_geom(points):
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
 line1 = None
